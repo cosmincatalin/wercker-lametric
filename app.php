8b0cfb7fb6c1186e6e85641982d9cfab30f731a3
Cosmin Catalin Sanda <cosmin@userreport.com>
2016-03-18 09:36:01 +0100

    Add more details about the problematic projects

--- app.php
+++ app.php
@@ -4,8 +4,8 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 try {
 
-    $username = trim(strtolower(preg_replace("/[^A-Za-z0-9\-]/", '', $_GET["username"])));
-    $token = trim(strtolower(preg_replace("/[^A-Za-z0-9]/", '', $_GET["token"])));
+    $username = trim(strtolower(preg_replace("/[^A-Za-z0-9\-]/", '', isset($argv[1]) ? $argv[1] : $_GET["username"])));
+    $token = trim(strtolower(preg_replace("/[^A-Za-z0-9]/", '', isset($argv[2]) ? $argv[2] : $_GET["token"])));
 
     if ( strlen($username) === 0 || strlen( $token) === 0 ) {
         $default = [
@@ -26,16 +26,27 @@ try {
         $buildsFailed = 0;
         $deploysFailed = 0;
 
+        $frames = [];
+        $frameNo = 1;
+
         foreach( $applications as $application ) {
             $applicationBuildsResource = $application["url"] . "/builds/?branch=master&status=finished&limit=1&token=$token";
             $applicationDeploysResource = $application["url"] . "/deploys/?status=finished&limit=1&token=$token";
             $builds = json_decode($client->request($method, $applicationBuildsResource)->getBody(), true);
             $deploys = json_decode($client->request($method, $applicationDeploysResource)->getBody(), true);
             foreach ( $builds as $build ) {
-                if ( $build["result"] !== "passed" ) $buildsFailed++;
+                if ( $build["result"] !== "passed" ) {
+                    $details = json_decode($client->request($method, $build["url"] . "?token=$token")->getBody(), true);
+                    array_push($frames, [ "index" => $frameNo++, "text" => $details["application"]["name"], "icon" => "i2514" ]);
+                    $buildsFailed++;
+                }
             }
             foreach ( $deploys as $deploy ) {
-                if ( $deploy["result"] !== "passed" ) $deploysFailed++;
+                if ( $deploy["result"] !== "passed" ) {
+                    $details = json_decode($client->request($method, $deploy["url"] . "?token=$token")->getBody(), true);
+                    array_push($frames, [ "index" => $frameNo++, "text" => $details["application"]["name"], "icon" => "i195" ]);
+                    $deploysFailed++;
+                }
             }
         }
 
@@ -48,22 +59,29 @@ try {
                 ]
             ];
         } else if ($buildsFailed !== 0 && $deploysFailed === 0) {
+
             $default = [
-                "frames" => [
-                    [ "index" => 0, "text" => "b $buildsFailed",  "icon" => "a609"]
-                ]
+                "frames" => array_merge(
+                    [
+                        [ "index" => 0, "text" => "b $buildsFailed",  "icon" => "a609"]
+                    ], $frames
+                )
             ];
         } else if ($buildsFailed === 0 && $deploysFailed !== 0) {
             $default = [
-                "frames" => [
-                    [ "index" => 0, "text" => "d $deploysFailed",  "icon" => "a609"]
-                ]
+                "frames" => array_merge(
+                    [
+                        [ "index" => 0, "text" => "d $deploysFailed",  "icon" => "a609"]
+                    ], $frames
+                )
             ];
         } else {
             $default = [
-                "frames" => [
-                    [ "index" => 0, "text" => "b $buildsFailed d $deploysFailed",  "icon" => "a609"]
-                ]
+                "frames" => array_merge(
+                    [
+                        [ "index" => 0, "text" => "b $buildsFailed d $deploysFailed",  "icon" => "a609"]
+                    ], $frames
+                )
             ];
         }
     }
