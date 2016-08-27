8e22c0318b8a3263952414d944c26acf39fdca12
Cosmin Catalin Sanda <cosmin@userreport.com>
2016-08-27 23:30:36 +0200

    Dorpping builds and deploys. Now looking only at runs.

--- app.php
+++ app.php
@@ -17,69 +17,41 @@ try {
         $method = "GET";
         $protocol = "https";
         $domain = "app.wercker.com";
-        $applicationsResource = "/api/v3/applications/" . $username;
 
         $client = new GuzzleHttp\Client();
 
-        $applications = json_decode($client->request($method, "$protocol://$domain/$applicationsResource/?token=$token")->getBody(), true);
+        $applications = json_decode($client->request($method, "$protocol://$domain/api/v3/applications/$username/?token=$token")->getBody(), true);
 
-        $buildsFailed = 0;
-        $deploysFailed = 0;
+        $runsFailed = 0;
 
         $frames = [];
         $frameNo = 1;
 
         foreach( $applications as $application ) {
-            $applicationBuildsResource = $application["url"] . "/builds/?branch=master&status=finished&limit=1&token=$token";
-            $applicationDeploysResource = $application["url"] . "/deploys/?status=finished&limit=1&token=$token";
-            $builds = json_decode($client->request($method, $applicationBuildsResource)->getBody(), true);
-            $deploys = json_decode($client->request($method, $applicationDeploysResource)->getBody(), true);
-            foreach ( $builds as $build ) {
-                if ( $build["result"] !== "passed" ) {
-                    $details = json_decode($client->request($method, $build["url"] . "?token=$token")->getBody(), true);
+            $applicationRunsResource = "$protocol://$domain/api/v3/runs/?applicationId=" . $application["id"] . "&branch=master&limit=1&status=finished&token=$token";
+            $runs = json_decode($client->request($method, $applicationRunsResource)->getBody(), true);
+            foreach ( $runs as $run ) {
+                if ( $run["result"] !== "passed" ) {
+                    $details = json_decode($client->request($method, $run["url"] . "?token=$token")->getBody(), true);
                     array_push($frames, [ "index" => $frameNo++, "text" => $details["application"]["name"] . " ", "icon" => "i2514" ]);
-                    $buildsFailed++;
-                }
-            }
-            foreach ( $deploys as $deploy ) {
-                if ( $deploy["result"] !== "passed" ) {
-                    $details = json_decode($client->request($method, $deploy["url"] . "?token=$token")->getBody(), true);
-                    array_push($frames, [ "index" => $frameNo++, "text" => $details["application"]["name"] . " ", "icon" => "i195" ]);
-                    $deploysFailed++;
+                    $runsFailed++;
                 }
             }
         }
 
         header("Content-Type: application/json");
 
-        if ( $buildsFailed === 0 && $deploysFailed === 0 ) {
+        if ( $runsFailed === 0 ) {
             $default = [
                 "frames" => [
                     [ "index" => 0, "text" => "healthy",  "icon" => "i59"]
                 ]
             ];
-        } else if ($buildsFailed !== 0 && $deploysFailed === 0) {
-
-            $default = [
-                "frames" => array_merge(
-                    [
-                        [ "index" => 0, "text" => "b $buildsFailed",  "icon" => "a609"]
-                    ], $frames
-                )
-            ];
-        } else if ($buildsFailed === 0 && $deploysFailed !== 0) {
-            $default = [
-                "frames" => array_merge(
-                    [
-                        [ "index" => 0, "text" => "d $deploysFailed",  "icon" => "a609"]
-                    ], $frames
-                )
-            ];
         } else {
             $default = [
                 "frames" => array_merge(
                     [
-                        [ "index" => 0, "text" => "b $buildsFailed d $deploysFailed",  "icon" => "a609"]
+                        [ "index" => 0, "text" => "$runsFailed",  "icon" => "a609"]
                     ], $frames
                 )
             ];
